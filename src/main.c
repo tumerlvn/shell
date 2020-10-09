f648e153ba349cece90521e3b795596be269983f
tumerlvn <tamer.makhmutov@gmail.com>
2020-10-09 09:06:37 +0600

    version 1.2.1: step 5 still does not work

--- src/main.c
+++ src/main.c
@@ -63,9 +63,9 @@ int checkRedirect(char **cmd) {
                 O_WRONLY | O_CREAT | O_TRUNC ,
                 S_IRUSR | S_IWUSR);
             dup2(fd, 1);
-            free(cmd[i]);//free to prevent SEGfault
-            cmd[i] = NULL;//needs to be done because execvp
-                          //reads command before meeting NULL
+            free(cmd[i]);       //free to prevent SEGfault
+            cmd[i] = NULL;      //needs to be done because execvp
+                                //reads command before meeting NULL
             return 1;
         } else if (strcmp(cmd[i], "<") == 0) {
             strcpy(fname, cmd[i+1]);
@@ -81,46 +81,108 @@ int checkRedirect(char **cmd) {
 }
 
 //to do this function
-void checkPipeline(char **cmd, int *x) {
+//x[0] == 0
+int checkPipeline(char **cmd, int **x) {
     int i = 0;
+    int cnt = 0;
     while (cmd[i]) {
-        
+        if (strcmp(cmd[i], "|") == 0) {
+            free(cmd[i]);
+            cmd[i] = NULL;
+            cnt++;
+            *x = realloc(*x, (cnt + 1) * sizeof(int));
+            (*x)[cnt] = i + 1;
+        }
         i++;
     }
+    return cnt;
 }
 
 int execute(char **cmd, int x) {
-    if (execvp(cmd[x], cmd) < 0) {
+    if (execvp(cmd[x], cmd + x) < 0) {
         perror("exec failed");
         return 1;
     }
     return 0;
 }
 
-void clearList(char **s) {
-    int i = 0;
-    while (s[i]) {
-        free(s[i]);
-        i++;
+void clearList(char **s, int *x, int cnt) {
+    for (int j = 0; j < cnt + 1; j++) {
+        int i = x[j];
+        while (s[i]) {
+            free(s[i]);
+            i++;
+        }
     }
     free(s);
 }
 
 int main() {
-    int x = 0;//from which place cmd is executed
+    int *x;
+    x = malloc(1 * sizeof(int));
+    x[0] = 0;//from which place cmd is executed
+
+    //int (*fd)[2] = malloc(sizeof(int[2]));
+    int fd[100][2];
+    int cnt;
     char **cmd = getList();
     while (!cmd[0] || (strcmp(cmd[0], "exit") && strcmp(cmd[0], "quit"))) {
         if (cmd[0]) {
-            if (fork() > 0) {
-                wait(NULL);
-            } else {
-                checkRedirect(cmd);
-                return execute(cmd, x);
+            cnt = checkPipeline(cmd, &x);
+             //if (cnt) fd = realloc(fd, (cnt + 1) * sizeof(int[2]));
+            for (int i = 0; i <= cnt; i++) {
+                pipe(fd[i]);
             }
+
+            if (fork() == 0) {
+                printf("cnt = %d\n", cnt);
+                if (cnt) dup2(fd[0][1], 1);
+                close(fd[0][1]);
+                close(fd[0][0]);
+
+                //checkRedirect(cmd);
+                return execute(cmd, x[0]);
+            } /*else {
+                waitpid(pid, NULL, 0);
+            }*/
+
+
+
+            for (int i = 1; i <= cnt; i++) {
+                if (fork() == 0) {
+                    dup2(fd[i - 1][0], 0);
+                    close(fd[i - 1][0]);
+                    close(fd[i - 1][1]);
+
+                        puts("1");
+                    if (i != cnt) {
+                        puts("2");
+                        dup2(fd[i][1], 1);
+                    }
+                    close(fd[i][1]);
+                    close(fd[i][0]);
+
+                    //checkRedirect(cmd);
+                    return execute(cmd, x[i]);
+                } /*else {
+                    waitpid(pid, NULL, 0);
+                }*/
+            }
+            for (int i = 0; i <= cnt; i++) {
+                //wait(NULL);
+                close(fd[i][0]);
+                close(fd[i][1]);
+            }
+//            fd = realloc(fd, sizeof(int[2]));
         }
-        clearList(cmd);//prevent segfault
+        clearList(cmd, x, cnt);//prevent segfault
+        free(x);
+        x = malloc(1 * sizeof(int));
+        x[0] = 0;
         cmd = getList();
     }
-    clearList(cmd);
+ //   free(fd);
+    clearList(cmd, x, 0);
+    free(x);
     return 0;
 }
